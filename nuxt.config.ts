2c5c3834a17042108d57f2b508bc4e1435f743c9
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-11-19 23:50:42 -0500

    🎈 perf: Redirection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import path from "path";
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
   css: ["~/assets/css/main.css"],
+  alias: {
+    "@": path.resolve(__dirname, "src"),
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
---
+++ pages/formations/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="flex lg:flex-row flex-col items-center justify-center gap-10 mb-5"
+  >
+    <FormationsBox title="GQS" />
+    <FormationsBox title="PSC1" />
+    <FormationsBox title="PSE1" />
+    <FormationsBox title="PSE2" />
+    <FormationsBox title="BNSSSA" />
+  </div>
+</template>
