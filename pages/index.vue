716b2f0449daa06678f90c35661519fb0a2ab986
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-11-25 22:33:14 -0500

    ✨ feat: Home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,60 @@
-<template></template>
+<template>
+  <svg
+    width="960"
+    height="540"
+    viewBox="0 0 960 540"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="none"
+    class="w-full h-full absolute top-0 left-0 -z-10"
+  >
+    <g clip-path="url(#clip0_187_2)">
+      <path d="M960 0H0V540H960V0Z" fill="url(#paint0_linear_187_2)" />
+      <path
+        d="M0 269L40 249.7C80 230.3 160 191.7 240 171.3C320 151 400 149 480 169.2C560 189.3 640 231.7 720 244C800 256.3 880 238.7 920 229.8L960 221V0H920C880 0 800 0 720 0C640 0 560 0 480 0C400 0 320 0 240 0C160 0 80 0 40 0H0V269Z"
+        fill="white"
+      />
+    </g>
+    <defs>
+      <linearGradient
+        id="paint0_linear_187_2"
+        x1="-7.72439e-06"
+        y1="371.5"
+        x2="960"
+        y2="369.5"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#F2282A" />
+        <stop offset="1" stop-color="#FE4A2B" />
+      </linearGradient>
+      <clipPath id="clip0_187_2">
+        <rect width="960" height="540" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+  <section
+    class="w-full h-1/2 absolute bottom-0 left-0 p-12 flex items-center lg:justify-between justify-center"
+  >
+    <h1 class="lg:text-9xl text-7xl opacity-75 font-semibold">
+      Sauver des vies
+    </h1>
+    <svg
+      width="6000"
+      height="6000"
+      viewBox="0 0 6000 6000"
+      fill="none"
+      class="w-48 h-48 shadow-lg rounded-3xl lg:block hidden"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="6000" height="6000" fill="#F2282A" />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M2750 1250C2694.77 1250 2650 1294.77 2650 1350V2650H1350C1294.77 2650 1250 2694.77 1250 2750V3250C1250 3305.23 1294.77 3350 1350 3350H2650V4650C2650 4705.23 2694.77 4750 2750 4750H3250C3305.23 4750 3350 4705.23 3350 4650V3350H4650C4705.23 3350 4750 3305.23 4750 3250V2750C4750 2694.77 4705.23 2650 4650 2650H3350V1350C3350 1294.77 3305.23 1250 3250 1250H2750Z"
+        fill="white"
+      />
+    </svg>
+  </section>
+</template>
+
+<style></style>
