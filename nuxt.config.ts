37a12dcc35a17b87b92ab5a737e0bf29ba094a68
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-11-30 01:14:24 -0500

    🐳 chore: Project settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 import path from "path";
 
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "nuxt-icon", "@nuxt/content"],
   css: ["~/assets/css/main.css"],
   alias: {
-    "@": path.resolve(__dirname, "src"),
+    "@content": path.resolve(__dirname, "content"),
   },
   postcss: {
     plugins: {
@@ -14,4 +17,5 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  ssr: false,
 });
