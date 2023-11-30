8377704a753aaf476164de8c4106d2bba03f6d97
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-11-30 01:15:21 -0500

    ✨ feat: Data API
    Init & Importing data

---
+++ server/api/PSC1.ts
@@ -0,0 +1,7 @@
+import data from "./PSC1.json";
+
+export default defineEventHandler(() => {
+  return {
+    data,
+  };
+});
---
+++ server/api/PSE1.ts
@@ -0,0 +1,7 @@
+import data from "./PSE1.json";
+
+export default defineEventHandler(() => {
+  return {
+    data,
+  };
+});
