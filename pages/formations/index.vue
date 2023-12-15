98f4dc34785eca0d81853caa75c7434d4b37f149
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-12-15 15:09:36 -0500

    📖 content: Finishing PSC1

--- pages/formations/index.vue
+++ pages/formations/index.vue
@@ -3,7 +3,7 @@
     class="flex lg:flex-row flex-col items-center justify-center gap-10 mb-5"
   >
     <FormationBox title="GQS" />
-    <!-- <FormationBox title="PSC1" />
+    <FormationBox title="PSC1" /><!--
     <FormationBox title="PSE1" />
     <FormationBox title="PSE2" /> -->
   </div>
